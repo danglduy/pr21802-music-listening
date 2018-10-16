887f0b96d0fd57f77c72f1e48e6500b1662f53d1
danglduy <38782010+danglduy@users.noreply.github.com>
2018-10-16 21:03:32 +0700

    Api music

---
+++ app/controllers/api/v1/albums_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class AlbumsController < ApplicationController
+      before_action :set_album, only: :show
+
+      def index
+        @albums = Album.all
+        respond_to do |format|
+          format.json
+        end
+      end
+
+      def show
+        respond_to do |format|
+          format.json
+        end
+      end
+
+      private
+      def set_album
+        @album = Album.find_by id: params[:id]
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get "player", to: "player#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :songs, only: [:index, :show]
+      resources :albums, only: [:index, :show]
+      resources :artists, only: [:index, :show]
+    end
+  end
 end
