7168f36b2632b030563b59a7cd99d292eb1bf7f9
danglduy <38782010+danglduy@users.noreply.github.com>
2018-10-24 08:41:25 +0000

    layout header, i18n

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,19 @@ Rails.application.routes.draw do
   get "player", to: "player#index"
 
   ActiveAdmin.routes(self)
-  devise_for :users, path: "", path_names: {sign_in: "login",
-    sign_out: "logout", edit: "profile"}
+  devise_for :users, path: "", path_names: {
+    sign_in: "login", sign_out: "logout", edit: "profile", sign_up: "signup"
+  }
+
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: "users",
+      controller: "devise/registrations",
+      as: :user_registration do
+        get :cancel
+      end
+  end
 
   resources :attachments, only: :destroy
 
